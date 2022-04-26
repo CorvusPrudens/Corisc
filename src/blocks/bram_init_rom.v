61e9735f537e496352f1305e51f5e7e71194b5f1
CorvusPrudens <corvusprudens@gmail.com>
2022-04-25 23:33:15 -0600

    Made init rom work for icestorm and verilog

--- src/blocks/bram_init_rom.v
+++ src/blocks/bram_init_rom.v
@@ -16,7 +16,11 @@ module bram_init_rom
 
   reg [(dataWidth_p - 1):0] memory [2**memSize_p-1:0];
 
+  `ifndef SIM
+  initial if (initFile_p) $readmemh(initFile_p, memory);
+  `else
   initial $readmemh(initFile_p, memory);
+  `endif
 
   always @(posedge clk_i) begin
     data_o <= memory[addr_i];
