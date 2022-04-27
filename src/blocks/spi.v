b7afbb63f4d95cd909e826ab36763e0edf113630
CorvusPrudens <corvusprudens@gmail.com>
2022-04-27 04:21:31 -0600

    Made SPI not use logic generated clock

--- src/blocks/spi.v
+++ src/blocks/spi.v
@@ -33,31 +33,41 @@ module spi(
   // a bit messy, but
   wire [3:0] rxState = spiState + 1'b1;
 
-  always @(posedge spi_clk_i) begin
-    case (spiState)
-      4'b0000: if (start) spiState <= 4'b0111;
-      4'b0111: spiState <= 4'b1110;
-      4'b1110: spiState <= 4'b1101;
-      4'b1101: spiState <= 4'b1100;
-      4'b1100: spiState <= 4'b1011;
-      4'b1011: spiState <= 4'b1010;
-      4'b1010: spiState <= 4'b1001;
-      4'b1001: spiState <= 4'b1000;
-      4'b1000: spiState <= 4'b1111;
-      4'b1111: spiState <= 4'b0000;
-      default: spiState <= 4'b0000;
-    endcase
+  reg edge_detect;
+  wire rising_edge = ~edge_detect & spi_clk_i;
+  wire falling_edge = edge_detect & ~spi_clk_i;
+  always @(posedge clk_i)
+    edge_detect <= spi_clk_i;
 
-    if (rxState[3]) shift_i[spiState[2:0]] <= sdi_i;
-    if (spiState == 4'b1111) data_o <= shift_i;
-  end
+  always @(posedge clk_i) begin
 
-  always @(negedge spi_clk_i) begin
-    if (busy_o) tx_bit <= shift_o[spiState[2:0]];
+    if (rising_edge) begin
+      case (spiState)
+        4'b0000: if (start) spiState <= 4'b0111;
+        4'b0111: spiState <= 4'b1110;
+        4'b1110: spiState <= 4'b1101;
+        4'b1101: spiState <= 4'b1100;
+        4'b1100: spiState <= 4'b1011;
+        4'b1011: spiState <= 4'b1010;
+        4'b1010: spiState <= 4'b1001;
+        4'b1001: spiState <= 4'b1000;
+        4'b1000: spiState <= 4'b1111;
+        4'b1111: spiState <= 4'b0000;
+        default: spiState <= 4'b0000;
+      endcase
+
+      if (rxState[3]) shift_i[spiState[2:0]] <= sdi_i;
+      if (spiState == 4'b1111) data_o <= shift_i;
+    end
+    
+    if (falling_edge) begin
+      if (busy_o) tx_bit <= shift_o[spiState[2:0]];
+    end
+
   end
 
   assign clk_active_o = spiState[3];
-  assign sck_o  = clk_active_o ? spi_clk_i : 1'b0;
+  assign sck_o  = clk_active_o ? edge_detect : 1'b0;
   assign sdo_o  = tx_bit;
   assign busy_o = spiState[3] | start;
   // assign cs_o   = spiState == 0;
