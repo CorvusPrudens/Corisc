ded198411ec5bade29ce18e5c251d483a77b7282
CorvusPrudens <corvusprudens@gmail.com>
2022-10-23 02:00:54 -0600

    Did some QSPI fixing

--- src/blocks/qspi.v
+++ src/blocks/qspi.v
@@ -35,23 +35,21 @@ module qspi #(
         input wire enable_qspi_i
     );
 
-    reg  [7:0] shifter;
+    reg  [8:0] shifter_in;
+    reg  [7:0] shifter_out;
     reg  [2:0] counter;
+    wire [3:0] counter_p1 = {1'b0, counter} + 1'b1;
     reg  [7:0] qpi_data_in;
     reg  [7:0] qpi_data_out;
-    wire [7:0] rev_shifter = {
-        shifter[0], shifter[1], shifter[2], shifter[3],
-        shifter[4], shifter[5], shifter[6], shifter[7]
-    };
 
-    assign data_o = enable_qspi_i ? qpi_data_in : rev_shifter;
+    assign data_o = enable_qspi_i ? qpi_data_in : shifter_in[8:1];
 
-    wire io0_data_o = counter[0] ? qpi_data_out[4] : qpi_data_out[0];
-    wire io1_data_o = counter[0] ? qpi_data_out[5] : qpi_data_out[1];
-    wire io2_data_o = counter[0] ? qpi_data_out[6] : qpi_data_out[2];
-    wire io3_data_o = counter[0] ? qpi_data_out[7] : qpi_data_out[3];
+    wire io0_data_o = qpi_data_out[0];
+    wire io1_data_o = qpi_data_out[1];
+    wire io2_data_o = qpi_data_out[2];
+    wire io3_data_o = qpi_data_out[3];
 
-    wire io0_out = enable_qspi_i ? io0_data_o : shifter[7];
+    wire io0_out = enable_qspi_i ? io0_data_o : shifter_out[7];
     wire io1_out = io1_data_o;
     wire io2_out = enable_qspi_i ? io2_data_o : 1'b1;
     wire io3_out = enable_qspi_i ? io2_data_o : 1'b1;
@@ -77,34 +75,87 @@ module qspi #(
     wire [3:0] nibble_in = {io3_sim_i, io2_sim_i, io1_sim_i, io0_sim_i};
     `endif
 
-    wire   clk_active = |counter[2:0];
+    reg [1:0] spi_sm;
+    wire   clk_active = |spi_sm;
     assign busy_o     = clk_active | start_i;
     assign sck_o      = clk_active & clk_i;
 
-    always @(posedge clk_i) begin
-        if (start_i) begin
-            counter <= counter + 1'b1;
-            shifter <= data_i;
-            qpi_data_out <= data_i;
-        end else if (busy_o) begin
+    always @(negedge clk_i) begin
+        case (spi_sm)
+            default:
+                if (start_i) begin
+                    shifter_out <= data_i;
 
-            if (enable_qspi_i && counter == 3'b010)
-                counter <= 0;
-            else
-                counter <= counter + 1'b1;
-            
-            `ifndef SIM
-            shifter <= {shifter[6:0], io1};
-            `else
-            shifter <= {shifter[6:0], io1_sim_i};
-            `endif
-            
-            if (counter[0])
-                qpi_data_in[7:4] <= nibble_in;
-            else
-                qpi_data_in[3:0] <= nibble_in;
-            
-        end
+                    qpi_data_out <= data_i;
+                end
+            2'b01:
+            begin
+                shifter_out <= {shifter_out[6:0], 1'b0};
+
+                qpi_data_out[3:0] <= qpi_data_out[7:4];
+            end
+        endcase
+    end
+
+    always @(posedge clk_i) begin
+        case (spi_sm)
+            default:
+            begin
+                if (start_i) begin
+                    // counter <= counter_p1;
+                    counter <= 0;
+                    // shifter <= {data_i[6:0], 1'b0};
+                    // qpi_data_out <= data_i;
+                    spi_sm <= 1;
+                end
+            end
+            2'b01:
+            begin
+
+                counter <= counter_p1[2:0];
+
+                if (enable_qspi_i & counter_p1[1])
+                    spi_sm <= 0;
+                else if (counter_p1[3])
+                    spi_sm <= 0;
+
+                `ifndef SIM
+                shifter_in <= {shifter_in[7:0], io1};
+                `else
+                shifter_in <= {shifter_in[7:0], io1_sim_i};
+                `endif
+
+                if (counter[0])
+                    qpi_data_in[7:4] <= nibble_in;
+                else
+                    qpi_data_in[3:0] <= nibble_in;
+            end
+        endcase
+
+
+        // if (start_i) begin
+        //     counter <= counter + 1'b1;
+        //     shifter <= data_i;
+        //     qpi_data_out <= data_i;
+        // end else if (busy_o) begin
+
+        //     if (enable_qspi_i && counter == 3'b010)
+        //         counter <= 0;
+        //     else
+        //         counter <= counter + 1'b1;
+
+        //     `ifndef SIM
+        //     shifter <= {shifter[6:0], io1};
+        //     `else
+        //     shifter <= {shifter[6:0], io1_sim_i};
+        //     `endif
+
+        //     if (counter[0])
+        //         qpi_data_in[7:4] <= nibble_in;
+        //     else
+        //         qpi_data_in[3:0] <= nibble_in;
+
+        // end
     end
 
 endmodule
